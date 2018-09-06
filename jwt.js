bf7b58e35f2e99e0c2d184057276e98ce8470548
claudiu9379 <office@bestdeveloper.ro>
2018-09-06 20:57:10 +0300

    sesione 17

--- jwt.js
+++ jwt.js
@@ -1,5 +1,26 @@
 const jwt = require("koa-jwt");
 const SECRET = "S3cRET~!";
 const jwtInstance = jwt({secret: SECRET});
+// Import jsonwebtoken
+const jsonwebtoken = require("jsonwebtoken");
 
-module.exports = jwtInstance;
+function JWTErrorHandler(ctx, next) {
+    return next().catch((err) => {
+            console.log("dddsbbbbbbbbbbbbbbbbbbbbbbbbb");
+            if (401 == err.status) {
+        ctx.status = 401;
+        ctx.body = {
+            "error": "Not authorized"
+        };
+    } else {
+        throw err;
+    }
+});
+};
+
+// helper function
+module.exports.issue =  (payload) => {
+    return jsonwebtoken.sign(payload, SECRET);
+};
+module.exports.jwt = () => jwtInstance;
+module.exports.errorHandler = () => JWTErrorHandler;
--- server.js
+++ server.js
@@ -4,26 +4,38 @@ const BodyParser = require("koa-bodyparser");
 const cryptoJS = require("node-cryptojs-aes").CryptoJS;
 const uuid = require("uuid");
 const encryption = require("./encryption")(cryptoJS, uuid);
-
+const jwt = require("./jwt");
 const app = new Koa();
-const router = new Router();
+//const router = new Router();
 const cors = require('@koa/cors');
 require("./mongo")(app);
+
+// Create a new securedRouter
+const router = new Router();
+const securedRouter = new Router();
+
+// Apply JWT middleware to secured router only
+securedRouter
+    //.use(jwt.errorHandler())
+    .use(jwt.jwt());
 
 app.use(BodyParser());
 app.use(cors());
 
 app.use(async (ctx, next) => {
     try{
-        ctx.body = await next();
+        console.log("000000000000");
+        const resp = await next();
+        console.log("response este dff", resp);
+        ctx.body = resp;
     }catch(ex){
         ctx.body = {err: ex.message || ex};
     }
-   
-    console.log('Setting status')
-    
+
+    // console.log('Setting status')
+
   })
-  app.use(require("./jwt"));
+  //app.use(require("./jwt"));
 
 router.get("/", async function (ctx) {
     ctx.body = {message: "Hello Worldsfdww1!"}
@@ -35,8 +47,38 @@ router.get("/people", async (ctx) => {
     
 });
 
-router.post("/test", async function (ctx) {
-    ctx.body = {message: "Hello Worldsfdww1!"}
+router.post("/test", async (ctx) => {
+    debugger;
+
+    const resp = {items:[],
+    count:0};
+        resp.items = await ctx.app.people.find().skip(0).limit(2    )
+    .toArray();
+    resp.count = await ctx.app.people.count();
+
+    console.log("ddddddddddddddddddddddddddddddd");
+    return  resp;
+});
+
+router.post("/auth", async (ctx) => {
+     console.log("aaa");
+   // throw "error";
+    let username = ctx.request.body.username;
+    let password = ctx.request.body.password;
+
+if (username === "user" && password === "pwd") {
+    return {
+        token: jwt.issue({
+            user: "user",
+            role: "admin"
+        })
+    }
+} else {
+    console.log("else");
+
+     ctx.status = 401;
+    return {error: "Invalid login"}
+}
 });
 
 router.post("/create", async function (ctx) {
@@ -64,6 +106,9 @@ router.post("/create", async function (ctx) {
     
 });
 
+// Add the securedRouter to our app as well
 app.use(router.routes()).use(router.allowedMethods());
+app.use(securedRouter.routes()).use(securedRouter.allowedMethods());
 
 app.listen(4001);
+
